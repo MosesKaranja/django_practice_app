c3d3ae83bba18f4d95c57cb12791249e89864284
MosesKaranja <moseskaranja830@gmail.com>
2024-01-15 11:29:13 +0300

    tests

--- myproject/polls/views.py
+++ myproject/polls/views.py
@@ -54,6 +54,25 @@ def vote(request, question_id):
         selected_choice.save()
         
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+    
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
+    
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+    
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
 
 
 # Create your views here.
